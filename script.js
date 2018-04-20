25eb7ff45c4853f84b5871e2af3aa5744420a116
Jegede Olamide <jegedeolamide99@gmail.com>
2018-04-20 21:52:44 +0100

    Add accordion script

---
+++ script.js
@@ -0,0 +1,13 @@
+var accordion = document.getElementsByClassName("accordion__toggle");
+var i;
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener("click",function() {
+    this.classList.toggle("active");
+    var accordionPanel = this.nextElementSibling;
+    if (accordionPanel.style.maxHeight) {
+      accordionPanel.style.maxHeight = null;
+    } else {
+      accordionPanel.style.maxHeight = accordionPanel.scrollHeight + "px";
+    }
+  });
+}
