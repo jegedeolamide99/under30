22d049090558cf65986d73ec1874a5f22b97a50b
Jegede Olamide <jegedeolamide99@gmail.com>
2018-05-16 09:15:35 +0100

    Minify js

--- script.js
+++ script.js
@@ -1,45 +1,8 @@
-var accordion = document.getElementsByClassName("accordion__toggle");
-var i;
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener("click",function() {
-    this.classList.toggle("active");
-    var accordionPanel = this.nextElementSibling;
-    if (accordionPanel.style.maxHeight) {
-      accordionPanel.style.maxHeight = null;
-    } else {
-      accordionPanel.style.maxHeight = accordionPanel.scrollHeight + "px";
-    }
-  });
-}
-function classToggle() {
-  const navs = document.querySelectorAll('.nav__links')
-
-  navs.forEach(nav => nav.classList.toggle('nav__toggleShow'));
-}
-document.querySelector('.nav__toggler')
-  .addEventListener('click', classToggle);
-// SLIDESHOW
-var slideIndex = 0;
-showSlides();
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "flex";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 7000); // Change image every 2 seconds
-}
-
+var accordion=document.getElementsByClassName("accordion__toggle");var i;for(i=0;i<accordion.length;i++){accordion[i].addEventListener("click",function(){this.classList.toggle("active");var accordionPanel=this.nextElementSibling;if(accordionPanel.style.maxHeight){accordionPanel.style.maxHeight=null;}else{accordionPanel.style.maxHeight=accordionPanel.scrollHeight+"px";}});}
+function classToggle(){const navs=document.querySelectorAll('.nav__links')
+navs.forEach(nav=>nav.classList.toggle('nav__toggleShow'));}
+document.querySelector('.nav__toggler').addEventListener('click',classToggle);var slideIndex=0;showSlides();function plusSlides(n){showSlides(slideIndex+=n);}
+function showSlides(){var i;var slides=document.getElementsByClassName("mySlides");var dots=document.getElementsByClassName("dot");for(i=0;i<slides.length;i++){slides[i].style.display="none";}
+slideIndex++;if(slideIndex>slides.length){slideIndex=1}
+for(i=0;i<dots.length;i++){dots[i].className=dots[i].className.replace(" active","");}
+slides[slideIndex-1].style.display="flex";dots[slideIndex-1].className+=" active";setTimeout(showSlides,7000);}
